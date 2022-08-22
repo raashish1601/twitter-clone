2b39efd084fe27bb7ff23b00d033be2ddd5f9422
Raashish Aggarwal <raashishaggarwal@gmail.com>
2022-08-22 17:26:26 +0530

    final commit

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,60 +1,44 @@
-import { db } from "../../firebase";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
-import { useRouter } from "next/router";
 import { getProviders, signIn } from "next-auth/react";
-export default function Signin({providers}) {
-  const router = useRouter();
-  const onGoogleClick = async () => {
-    try {
-      const auth = getAuth();
-      const provider = new GoogleAuthProvider();
-      await signInWithPopup(auth, provider);
-      const user = auth.currentUser.providerData[0];
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
-      if (!docSnap.exists()) {
-        await setDoc(docRef, {
-          name: user.displayName,
-          email: user.email,
-          username: user.displayName.split(" ").join("").toLocaleLowerCase(),
-          userImg: user.photoURL,
-          uid: user.uid,
-          timestamp: serverTimestamp(),
-        });
-      }
-      router.push("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+
+export default function Signin({ providers }) {
   return (
     <div className="flex justify-center mt-20 space-x-4">
       <img
-        src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
+        src="https://img.freepik.com/free-vector/big-tree-isolated-cartoon_1308-111956.jpg?w=2000"
         alt="twitter image inside a phone"
-        className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
+        className="hidden object-cover md:w-[340px]  md:inline-flex"
       />
       <div className="">
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name} className="flex flex-col items-center">
-          <img
-            className="w-36 object-cover"
-            src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-            alt="twitter logo"
-          />
-          <p className="text-center text-sm italic my-10">
-            This app is created for learning purposes
-          </p>
-          <button
-            onClick={onGoogleClick}
-            className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-          >
-            Sign in with Google
-          </button>
-        </div>
-          ))}
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col items-center">
+            <img
+              className="w-36 object-cover"
+              src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+              alt="twitter logo"
+            />
+            <p className="text-center text-sm italic my-20 font-bold">
+              Created by Ankit
+            </p>
+            <button
+              // onClick={onGoogleClick}
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500 hover:font-bold"
+            >
+              Sign in with Google
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
+}
+
+// getting providers on server side
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
 }
